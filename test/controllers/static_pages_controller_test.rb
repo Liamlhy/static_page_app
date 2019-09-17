d5e5fa6a90367b521ee086aba6c70e0541d5906d
Liamlhy <liamlilhy@gmail.com>
2019-09-16 23:15:27 -0500

    modifications

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+    @base_title = "My Individual Project"
   end
 
   test "should get home" do
