6fd2aafbd9a7cfff69ea55f3abf36d7f0c527d75
Liamlhy <liamlilhy@gmail.com>
2019-09-24 21:26:58 -0500

    HW4

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,15 @@
 class StaticPagesController < ApplicationController
   def home
+    @result = (1..6).to_a.shuffle.first
   end
 
   def help
   end
 
   def about
+  end
+
+  def roll
+     @result = (0..100).to_a.shuffle.first
   end
 end
