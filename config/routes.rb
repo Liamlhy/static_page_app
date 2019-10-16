90d78dda400c3a62363f705a71ff36192c4d0b3a
Liamlhy <liamlilhy@gmail.com>
2019-10-16 10:40:10 -0500

    start working on signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  # get 'users/new'
   root 'static_pages#home'
-  get  'static_pages/home'
-  get  'static_pages/help'
-  get  'static_pages/about'
-  get  'static_pages/roll'
+  get '/help' to: 'static_pages#help'
+  get '/about' to: 'static_pages#about'
+  get '/roll' to: 'static_pages#roll'
+  # get  'static_pages/home'
+  # get  'static_pages/help'
+  # get  'static_pages/about'
+  # get  'static_pages/roll'
 end
